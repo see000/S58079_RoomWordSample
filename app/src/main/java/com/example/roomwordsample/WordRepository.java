432427555fed75f4fc3ab034a6d7feeed9adfc1a
Son <son123@gmail.com>
2023-01-04 19:46:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/WordRepository.java
@@ -0,0 +1,42 @@
+package com.example.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import android.app.Application;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+/**
+ * Abstracted Repository as promoted by the Architecture Guide.
+ * https://developer.android.com/topic/libraries/architecture/guide.html
+ */
+
+class WordRepository {
+
+    private WordDao mWordDao;
+    private LiveData<List<Word>> mAllWords;
+
+
+    WordRepository(Application application) {
+        WordRoomDatabase db = WordRoomDatabase.getDatabase(application);
+        mWordDao = db.wordDao();
+        mAllWords = mWordDao.getAlphabetizedWords();
+    }
+
+
+    LiveData<List<Word>> getAllWords() {
+        return mAllWords;
+    }
+
+
+    void insert(Word word) {
+        WordRoomDatabase.databaseWriteExecutor.execute(() -> {
+            mWordDao.insert(word);
+        });
+    }
+}
